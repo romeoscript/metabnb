12afc7fbc6fa29ec73700199a9f8475d8434c391
Romeoscript <romeobourne211@gmail.com>
2022-11-14 11:56:11 +0100

    feat:added the images fetched from data js

--- src/components/HomeContent.jsx
+++ src/components/HomeContent.jsx
@@ -2,35 +2,41 @@ import React from "react";
 import "../Assets/css/Homecontent.scss";
 import frame from "../Assets/images/Frame 151 (1).png";
 import { AiFillStar } from "react-icons/ai";
-
+import datas from './data'
+console.log(datas)
 const HomeContent = () => {
   return (
     <div className="home__content">
       <h2>Inspiration for your next adventure</h2>
       {/* image items */}
       <div className="home__content-grid">
-        <aside>
-          <div className="image_cont">
-            <img src={frame} alt="" />
-          </div>
-          <div className="text_content-grid">
-            <span>
-              <p>Desert king</p>
-              <p> <strong>1MBT per night</strong></p>
-            </span>
-            <span>
-              <p>2345km away</p>
-              <p>available for 2weeks stay</p>
-            </span>
-          </div>
-          <span className="span__icons">
-            <AiFillStar className="aicons" />
-            <AiFillStar className="aicons"/>
-            <AiFillStar className="aicons"/>
-            <AiFillStar className="aicons"/>
-            <AiFillStar className="aicons"/>
-          </span>
-        </aside>
+        {datas.slice(0,8).map(data =>{
+            return(
+                <aside>
+                <div className="image_cont">
+                  <img src={data.image} alt="" />
+                </div>
+                <div className="text_content-grid">
+                  <span>
+                    <p>{data.text}</p>
+                    <p> <strong>{data.ptext}</strong></p>
+                  </span>
+                  <span>
+                    <p>2345km away</p>
+                    <p>available for 2weeks stay</p>
+                  </span>
+                </div>
+                <span className="span__icons">
+                  <AiFillStar className="aicons" />
+                  <AiFillStar className="aicons"/>
+                  <AiFillStar className="aicons"/>
+                  <AiFillStar className="aicons"/>
+                  <AiFillStar className="aicons"/>
+                </span>
+              </aside>
+            )
+        })}
+       
       </div>
       {/* image items end */}
     </div>
--- src/components/data.js
+++ src/components/data.js
@@ -1,3 +1,104 @@
-// const data =[
+import frame from '../Assets/images/Frame 151.png'
+import frame1 from '../Assets/images/Frame 151 (1).png'
+import frame2 from '../Assets/images/Frame 151 (2).png'
+import frame3 from '../Assets/images/Frame 151 (3).png'
+import frame4 from '../Assets/images/Frame 151 (4).png'
+import frame5 from '../Assets/images/Frame 151 (5).png'
+import frame6 from '../Assets/images/Frame 151 (6).png'
+import frame7 from '../Assets/images/Frame 151 (7).png'
+import frame9 from '../Assets/images/Frame 151 (18).png'
+import frame10 from '../Assets/images/Frame 151 (9).png'
+import frame11 from '../Assets/images/Frame 151 (10).png'
+import frame12 from '../Assets/images/Frame 151 (11).png'
+import frame13 from '../Assets/images/Frame 151 (12).png'
+import frame14 from '../Assets/images/Frame 151 (13).png'
+import frame15 from '../Assets/images/Frame 151 (14).png'
+import frame16 from '../Assets/images/Frame 151 (15).png'
 
-// ]
+
+
+ const data =[
+    {
+        image:frame,
+        text:'Desert king',
+        ptext:'1MBT per night'
+    },
+    {
+        image:frame1,
+        text:'Desert king',
+        ptext:'1MBT per night'
+    },
+    {
+        image:frame2,
+        text:'Desert king',
+        ptext:'1MBT per night'
+    },
+    {
+        image:frame3,
+        text:'Desert king',
+        ptext:'1MBT per night'
+    },
+    {
+        image:frame4,
+        text:'Desert king',
+        ptext:'1MBT per night'
+    },
+    {
+        image:frame5,
+        text:'Desert king',
+        ptext:'1MBT per night'
+    },
+    {
+        image:frame6,
+        text:'Desert king',
+        ptext:'1MBT per night'
+    },
+   
+    {
+        image:frame7,
+        text:'Desert king',
+        ptext:'1MBT per night'
+    },
+    {
+        image:frame9,
+        text:'Desert king',
+        ptext:'1MBT per night'
+    },
+    {
+        image:frame10,
+        text:'Desert king',
+        ptext:'1MBT per night'
+    },
+    {
+        image:frame11,
+        text:'Desert king',
+        ptext:'1MBT per night'
+    },
+    {
+        image:frame12,
+        text:'Desert king',
+        ptext:'1MBT per night'
+    },
+    {
+        image:frame13,
+        text:'Desert king',
+        ptext:'1MBT per night'
+    },
+    {
+        image:frame14,
+        text:'Desert king',
+        ptext:'1MBT per night'
+    },
+    {
+        image:frame15,
+        text:'Desert king',
+        ptext:'1MBT per night'
+    },
+    {
+        image:frame16,
+        text:'Desert king',
+        ptext:'1MBT per night'
+    },
+  
+]
+export default data;

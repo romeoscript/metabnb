1d1d78227b445ce67ae04ff2f423074e2a008d7d
Romeoscript <romeobourne211@gmail.com>
2022-11-14 11:01:24 +0100

    feat:newfeatures

--- src/components/HomeContent.jsx
+++ src/components/HomeContent.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "../Assets/css/Homecontent.scss";
 import frame from "../Assets/images/Frame 151 (1).png";
-import { AiFillStar } from 'react-icons/ai'
+import { AiFillStar } from "react-icons/ai";
 
 const HomeContent = () => {
   return (
@@ -14,12 +14,22 @@ const HomeContent = () => {
             <img src={frame} alt="" />
           </div>
           <div className="text_content-grid">
-            <p>Desert king</p>
-            <p>1MBT per night</p>
-            <p>2345km away</p>
-            <p>available for 2weeks stay</p>
+            <span>
+              <p>Desert king</p>
+              <p> <strong>1MBT per night</strong></p>
+            </span>
+            <span>
+              <p>2345km away</p>
+              <p>available for 2weeks stay</p>
+            </span>
           </div>
-          <span><AiFillStar/><AiFillStar/><AiFillStar/><AiFillStar/><AiFillStar/></span>
+          <span className="span__icons">
+            <AiFillStar className="aicons" />
+            <AiFillStar className="aicons"/>
+            <AiFillStar className="aicons"/>
+            <AiFillStar className="aicons"/>
+            <AiFillStar className="aicons"/>
+          </span>
         </aside>
       </div>
       {/* image items end */}
